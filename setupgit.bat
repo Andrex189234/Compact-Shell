6efd557632b9f9cee054b3f4b6ccba2bf623fd39
Andrea <165200977+Andrex189234@users.noreply.github.com>
2026-02-24 19:16:40 +0100

    Add files via upload

--- setupgit.bat
+++ setupgit.bat
@@ -17,20 +17,20 @@ echo.
 echo L'utente predefinito e "guest" e la password predefinita e "guest". Puoi modificare queste credenziali nel file "users.cfg" che si trova nella stessa directory di questo script di installazione.
 echo Per creare un nuovo utente, aggiungi semplicemente una nuova riga nel file "users.cfg" con il formato "username=password". Ad esempio, per creare un utente chiamato "admin" con password "admin123", dovresti aggiungere la riga "admin=admin123" al file.
 notepad "users.cfg"
-set /p launch=Do you want to launch the Compact Shell? (y/n):
-if /i "%launch%"=="y" start "" "C:\Users\%USERNAME%\Documents\Starting\Italian.bat"
-if /i "%launch%"=="n" exit
+pause
+exit
 
 :ENGLISH
 echo You selected English.
 echo The program will now clone the Compact Shell GitHub repository. Please wait...
 git clone https://github.com/Andrex189234/Compact-Shell.git C:\Users\%USERNAME%\Documents\Starting
 cd C:\Users\%USERNAME%\Documents\Starting
+echo.
 echo The program will now create a shortcut on your desktop. Please wait...
 copy "C:\Users\%USERNAME%\Documents\Starting\English.bat" "%USERPROFILE%\Desktop\Compact Shell - English.bat"
+echo.
 echo The default user is "guest" and the default password is "guest". You can change these credentials in the "users.cfg" file located in the same directory as this setup script.
 echo To create a new user, simply add a new line in the "users.cfg" file with the format "username=password". For example, to create a user named "admin" with the password "admin123", you would add the line "admin=admin123" to the file.
 notepad "users.cfg"
-set /p launch=Do you want to launch the Compact Shell? (y/n):
-if /i "%launch%"=="y" start "" "C:\Users\%USERNAME%\Documents\Starting\English.bat"
-if /i "%launch%"=="n" exit
+pause
+exit
